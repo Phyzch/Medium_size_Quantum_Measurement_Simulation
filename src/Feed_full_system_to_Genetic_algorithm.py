ba814c024c591e85461ea47bf3b679caaf6ebd46
Chenghao Zhang <cz38@illinois.edu>
2021-12-21 15:24:29 -0600

    finish simplify Feed_full_system_to_Genetic_algorith.py

--- src/Feed_full_system_to_Genetic_algorithm.py
+++ src/Feed_full_system_to_Genetic_algorithm.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from include.full_system_class.Full_system_class import full_system
 
 from include.genetic_algorithm_class.Genetic_algorithm_class import Extend_Genetic_algorithm
-from Fitness_function import fitness_function , simulate_full_system_energy_flow, Analyze_peak_and_peak_duration , fit_func1
+from Fitness_function import fitness_function , simulate_full_system_energy_flow, Analyze_peak_and_peak_duration , fit_func1 , compute_coupling_geometric_mean
 
 from include.util import Broadcast_data
 from mpi4py import MPI
@@ -15,84 +15,83 @@ import os
 
 
 def plot_simulation_result( *args ):
-    Time_list , photon_energy_list, d1_energy_list_change, d2_energy_list_change , file_path = args
-    # plot simulation result
+    photon_energy_list, d1_energy_list_change, d2_energy_list_change ,Time_list , file_path = args
+    # configure fig, ax
     fig1, ax1 = plt.subplots(nrows=1, ncols=1)
     ax1.plot(Time_list, d1_energy_list_change, label='left photon localization')
     ax1.plot(Time_list, d2_energy_list_change, label='right photon localization')
     ax1.plot(Time_list, photon_energy_list, label='photon energy')
 
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('E')
+
     ax1.legend(loc='best')
 
+    # save figure.
     fig_name = "best_simulation_result.png"
     fig_name = os.path.join(file_path, fig_name)
 
     fig1.savefig(fig_name)
 
+def write_data(f, data_list , symbol):
+    f.write(symbol + "\n")
+    for data in data_list:
+        f.write(str(  round(data,4) ) + " ")
+    f.write("\n")
+
 def save_simulation_result(*args):
-    best_param , best_fitness, parameter_number, coupling_strength, Time_list, \
-    First_peak_Time_duration_best_param , max_energy_change_best_param , Localization_duration_ratio_best_param ,\
+    best_param , best_fitness, parameter_number, parameter_range, Time_list, \
+    analyze_result,\
     d1_energy_list_change , d2_energy_list_change , photon_energy_list , file_path = args
+
+    first_peak_time_duration_ratio_best_param, max_energy_change_best_param, localization_duration_ratio_best_param , _ = analyze_result
 
     # save best simulation result found
     filename1 = 'best_parameter_and_fitness_function.txt'
     filename1 = os.path.join(file_path, filename1)
     with open(filename1, 'w') as f1:
 
+        # record parameter we fit.
         for num in best_param:
             f1.write(str(num) + " , ")
         f1.write('\n')
 
         # compute geometric mean and write to output file.
-        parameter_set_geometric_mean = 1
-
-        for parameter in best_param:
-            parameter_set_geometric_mean = parameter_set_geometric_mean * np.power(np.abs(parameter),
-                                                                                   1 / parameter_number)
-
-        parameter_set_geometric_mean_ratio = parameter_set_geometric_mean / coupling_strength
+        normalized_geometric_mean = compute_coupling_geometric_mean(best_param, parameter_number, parameter_range)
 
         f1.write('geometric mean : \n')
-        f1.write(str(parameter_set_geometric_mean_ratio) + " \n")
+        f1.write(str(normalized_geometric_mean) + " \n")
 
         f1.write('best fitness ' + "\n")
         f1.write(str(best_fitness) + '\n')
 
-        best_fitness, Max_energy_fitness_contribution, Localization_duration_ratio_contribution, First_peak_duration_contribution = fit_func1(
-            First_peak_Time_duration_best_param,
+        # fit function
+        best_fitness_by_fitting, max_energy_fitness_contribution, localization_duration_ratio_contribution, first_peak_duration_contribution = fit_func1(
+            first_peak_time_duration_ratio_best_param,
             max_energy_change_best_param,
-            Localization_duration_ratio_best_param,
-            parameter_set_geometric_mean_ratio)
+            localization_duration_ratio_best_param,
+            normalized_geometric_mean)
+
+        # assert the fitness function we get is correct.
+        fitness_func_diff = np.abs(best_fitness - best_fitness_by_fitting)/np.abs(best_fitness)
+        assert( fitness_func_diff < 0.05 )
+
         f1.write(
-            "contribution from  1. Max energy  2. localization duration ratio  3. first peak duartion:   " + "\n")
-        f1.write(str(Max_energy_fitness_contribution) + "  ,  " + str(
-            Localization_duration_ratio_contribution) + "  , " + str(First_peak_duration_contribution) + "\n")
+            "contribution from  1. Max energy  2. localization duration ratio  3. first peak duartion_ratio:   " + "\n")
+        f1.write(str(max_energy_fitness_contribution) + "  ,  " + str(
+            localization_duration_ratio_contribution) + "  , " + str(first_peak_duration_contribution) + "\n")
 
-        f1.write('First_peak_Time_duration, max_energy_change, Localization_duration ' + "\n")
+        f1.write('first_peak_Time_duration_ratio, max_energy_change, localization_duration ' + "\n")
 
-        f1.write(str(First_peak_Time_duration_best_param) + "  " + str(max_energy_change_best_param) + "  " + str(
-            Localization_duration_ratio_best_param) + "\n")
+        f1.write(str(first_peak_time_duration_ratio_best_param) + "  " + str(max_energy_change_best_param) + "  " + str(
+            localization_duration_ratio_best_param) + "\n")
 
-        f1.write('Time: ' + '\n')
 
-        for t in Time_list:
-            f1.write(str(t) + " ")
-        f1.write('\n')
-
-        f1.write('el:  ' + '\n')
-        for energy in d1_energy_list_change:
-            f1.write(str(round(energy, 4)) + " ")
-        f1.write('\n')
-
-        f1.write('er:  ' + '\n')
-        for energy in d2_energy_list_change:
-            f1.write(str(round(energy, 4)) + "  ")
-        f1.write('\n')
-
-        f1.write('e_photon' + "\n")
-        for energy in photon_energy_list:
-            f1.write(str(round(energy, 4)) + "  ")
-        f1.write('\n')
+        # record : time, el, er, energy.
+        write_data(f1, Time_list , 'Time: ')
+        write_data(f1, d1_energy_list_change , "el:  ")
+        write_data(f1, d2_energy_list_change , "er:  ")
+        write_data(f1, photon_energy_list , "e_photon:  ")
 
 def Evaluate_simulation_result( *args ):
     ga, full_system_instance , file_path , population_size, coupling_strength , parameter_number, highest_peak_bool = args
@@ -100,15 +99,14 @@ def Evaluate_simulation_result( *args ):
     # ------------------  Evaluate result of Genetic algorithm ----------------------
 
     # output last generations and their fitfunction.
-    Last_generation = ga.last_generation()
+    last_generation = ga.last_generation()
     # (member.fitness, member.genes)
-    Last_generation_fitness_func = [member[0] for member in Last_generation]
-    Last_generation = ga.last_generation()
-    Last_generation_param = [member[1] for member in Last_generation]
+    last_generation_fitness_func = [member[0] for member in last_generation]
+    last_generation_param = [member[1] for member in last_generation]
 
     # Broadcast fitness function and paramter to all process
-    parameter_for_all  = Broadcast_data(Last_generation_param , num_proc )
-    fitness_for_all = Broadcast_data( np.real(Last_generation_fitness_func) , num_proc )
+    parameter_for_all = Broadcast_data(last_generation_param , num_proc )
+    fitness_for_all = Broadcast_data( np.real(last_generation_fitness_func) , num_proc )
 
 
     if rank == 0:
@@ -116,29 +114,29 @@ def Evaluate_simulation_result( *args ):
         print('num of proc: ' + str(num_proc))
         print('population size: ' + str(population_size))
 
-        best_index = np.argmax(fitness_for_all)
-        best_fitness = fitness_for_all[best_index]
-
-        best_param = parameter_for_all[best_index]
+        best_gene_index = np.argmax(fitness_for_all)
+        best_fitness = fitness_for_all[best_gene_index]
+        best_param = parameter_for_all[best_gene_index]
 
         # Sort parameter set according to their fitness function.
-        Sort_index = np.argsort(-fitness_for_all)
-        parameter_for_all = parameter_for_all[Sort_index]
-        fitness_for_all = fitness_for_all[Sort_index]
+        sort_index = np.argsort(-fitness_for_all)
+        parameter_for_all = parameter_for_all[sort_index]
+        fitness_for_all = fitness_for_all[sort_index]
 
         # plot best parameter simulation result
-        photon_energy_list, d1_energy_list_change, d2_energy_list_change, Time_list = simulate_full_system_energy_flow(
+        photon_energy_list, d1_energy_list_change, d2_energy_list_change, time_list = simulate_full_system_energy_flow(
             full_system_instance, best_param)
 
-        First_peak_Time_duration_best_param, max_energy_change_best_param, Localization_duration_ratio_best_param, localization_bool = \
-            Analyze_peak_and_peak_duration( d1_energy_list_change, d2_energy_list_change, Time_list , highest_peak_bool= highest_peak_bool)
-
         # plot simulation result
-        plot_simulation_result(Time_list , photon_energy_list, d1_energy_list_change, d2_energy_list_change , file_path )
+        plot_simulation_result( photon_energy_list, d1_energy_list_change, d2_energy_list_change , time_list , file_path )
+
+        # analyze result
+        analyze_result = \
+            Analyze_peak_and_peak_duration( d1_energy_list_change, d2_energy_list_change, time_list , highest_peak_bool= highest_peak_bool)
 
         # Now you have to save result to files:
-        save_simulation_result(    best_param , best_fitness, parameter_number, coupling_strength, Time_list,
-            First_peak_Time_duration_best_param , max_energy_change_best_param , Localization_duration_ratio_best_param ,
+        save_simulation_result(  best_param , best_fitness, parameter_number, coupling_strength, time_list,
+            analyze_result,
             d1_energy_list_change , d2_energy_list_change , photon_energy_list , file_path)
 
 
--- src/Fitness_function.py
+++ src/Fitness_function.py
@@ -45,12 +45,12 @@ def analyze_peak_property(time, energy_change, energy_change_peak_index):
     first_peak_time = np.array(time)[energy_change_first_peak_index]
     # time_duration.
     first_peak_time_duration = (first_peak_time[-1] - first_peak_time[0]) if first_peak_len > 0 else 0
-
+    first_peak_time_duration_ratio = first_peak_time_duration / final_time
     # maximum energy change for first peak. We set maximum energy change for first peak and max energy change.
     max_energy_change_first_peak = max(energy_change_first_peak) if first_peak_len !=0 else 0
 
 
-    return first_peak_time_duration, max_energy_change_first_peak , localization_duration_ratio
+    return first_peak_time_duration_ratio , max_energy_change_first_peak , localization_duration_ratio
 
 def decide_localization_side(highest_peak_bool, max_energy_change , max_e2l_change , max_e2r_change, e2l_change_peak_index ,  e2r_change_peak_index ):
     if(highest_peak_bool):
@@ -108,41 +108,12 @@ def Analyze_peak_and_peak_duration(e2l_change, e2r_change, time , highest_peak_b
 
     if localization_bool == 1:
         # localize to left
-        first_peak_time_duration, max_energy_change , localization_duration_ratio = analyze_peak_property(time, e2l_change, e2l_change_peak_index)
+        first_peak_time_duration_ratio, max_energy_change , localization_duration_ratio = analyze_peak_property(time, e2l_change, e2l_change_peak_index)
     else:
-        first_peak_time_duration, max_energy_change , localization_duration_ratio = analyze_peak_property(time, e2r_change, e2r_change_peak_index)
+        first_peak_time_duration_ratio, max_energy_change , localization_duration_ratio = analyze_peak_property(time, e2r_change, e2r_change_peak_index)
 
 
-    return first_peak_time_duration, max_energy_change,localization_duration_ratio, localization_bool
-
-def fit_func1(First_peak_Time_duration, max_energy_change ,  Localization_duration_ratio, parameter_geometric_mean_ratio):
-    '''
-    see Genetic_algorithm_fitness_func.py : fit_func for requirement of fit_func
-    # We need to be very careful when defining fitness function.
-
-    Localization_duration_ratio is in range[0,1], max_energy_change is also in range[0,1]
-
-    parameter_geometric_mean_ratio is geometric mean of parameter / upper limit you set  for parameter.
-    This is to make sure we don't sample extremely small parameter to get long localization time.
-    :return:
-
-    '''
-    # ratio of first peak duration.
-    Time_duration = include.Shared_data.Time_duration
-
-    First_peak_Time_duration_ratio = First_peak_Time_duration / Time_duration * parameter_geometric_mean_ratio
-
-    # Fitness_func_value = 0.5 * pow(max_energy_change, 2 ) + 10 * pow(Localization_duration_ratio , 2) + 60 * First_peak_Time_duration_ratio
-
-    Max_energy_fitness_contribution = 5 * pow(max_energy_change, 2 )
-
-    Localization_duration_ratio_contribution = 10 * pow(Localization_duration_ratio , 2)
-
-    First_peak_duration_contribution = First_peak_Time_duration_ratio * 60
-
-    Fitness_func_value = Max_energy_fitness_contribution + Localization_duration_ratio_contribution + First_peak_duration_contribution
-
-    return Fitness_func_value , Max_energy_fitness_contribution, Localization_duration_ratio_contribution , First_peak_duration_contribution
+    return first_peak_time_duration_ratio, max_energy_change,localization_duration_ratio, localization_bool
 
 def simulate_full_system_energy_flow(full_system_instance, off_diagonal_coupling):
     # construct off diagonal part for Hamiltonian
@@ -166,6 +137,7 @@ def simulate_full_system_energy_flow(full_system_instance, off_diagonal_coupling
 
     return photon_energy_list, d1_energy_list_change, d2_energy_list_change, time_list
 
+
 def fitness_function(off_diagonal_coupling , param  ):
      # data is maximum range of coupling strength
      parameter_range , full_system_instance, parameter_number , highest_peak_bool  = param
@@ -173,18 +145,49 @@ def fitness_function(off_diagonal_coupling , param  ):
      # construct off diagonal part of Hamiltonian. and initialize wave function
      photon_energy_list, d1_energy_list_change, d2_energy_list_change, time_list = simulate_full_system_energy_flow( full_system_instance, off_diagonal_coupling)
 
-     first_peak_time_duration, max_energy_change, localization_duration_ratio, localization_bool = \
+     first_peak_time_duration_ratio, max_energy_change, localization_duration_ratio, localization_bool = \
          Analyze_peak_and_peak_duration(d1_energy_list_change, d2_energy_list_change, time_list, highest_peak_bool= highest_peak_bool )
 
      # compute geometric mean and its ratio to coupling strength.
-     geometric_mean = 1
-     for i in range(parameter_number):
-         geometric_mean = geometric_mean * pow(off_diagonal_coupling[i] , 1/parameter_number)
+     normalized_geometric_mean = compute_coupling_geometric_mean(off_diagonal_coupling , parameter_number , parameter_range)
 
-
-     geometric_mean_ratio = geometric_mean / parameter_range
-
-     fitness_func_value , Max_energy_fitness_contribution, Localization_duration_ratio_contribution , First_peak_duration_contribution  = fit_func1(first_peak_time_duration , max_energy_change, localization_duration_ratio,
-                                   geometric_mean_ratio)
+     fitness_func_value , Max_energy_fitness_contribution, Localization_duration_ratio_contribution , First_peak_duration_contribution  = fit_func1(first_peak_time_duration_ratio , max_energy_change, localization_duration_ratio,
+                                   normalized_geometric_mean)
 
      return fitness_func_value
+
+def compute_coupling_geometric_mean(off_diagonal_coupling , parameter_number, parameter_range):
+    # pow(  \prod coupling / parameter_range , 1/N)
+    geometric_mean = 1
+    for i in range(parameter_number):
+        geometric_mean = geometric_mean * pow(off_diagonal_coupling[i], 1 / parameter_number)
+    normalized_geometric_mean = geometric_mean / parameter_range
+
+    return normalized_geometric_mean
+
+def fit_func1(first_peak_Time_duration_ratio, max_energy_change, Localization_duration_ratio, parameter_geometric_mean_ratio):
+    '''
+    We need to be very careful when defining fitness function.
+
+    localization_duration_ratio is in range[0,1], max_energy_change is in range[0,1],  first_peak_time_duration_ratio is also in range[0,1]
+
+    parameter_geometric_mean_ratio is geometric mean of parameter / upper limit you set  for parameter.
+    This is to make sure we don't sample extremely small parameter to get long localization time.
+
+    This choice of fitness function is tricky and ad-hoc, try different choices to see which works best.
+    See p.97 in Practical Genetic algorithms (R.L.Haupt & S.E.Haupt) about choosing fitness function for multi-objective optimization.
+    :return:
+
+    '''
+    # ratio of first peak duration. scaled by geometric mean value of parameter as t~1/V, we want to avoid optimized to small parameter.
+    scaled_first_peak_time_duration_ratio = first_peak_Time_duration_ratio  * parameter_geometric_mean_ratio
+
+    max_energy_fitness_contribution = 0.5 * pow(max_energy_change, 2 )
+
+    localization_duration_ratio_contribution = 1 * pow(Localization_duration_ratio , 2)
+
+    first_peak_duration_contribution = scaled_first_peak_time_duration_ratio * 6
+
+    fitness_func_value = max_energy_fitness_contribution + localization_duration_ratio_contribution + first_peak_duration_contribution
+
+    return fitness_func_value , max_energy_fitness_contribution, localization_duration_ratio_contribution , first_peak_duration_contribution
